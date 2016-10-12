91a18398e0cc20e3d5ec8a7e4b5b7452acee7444
bpainto <bpainto@gmail.com>
2016-10-11 22:27:24 -0400

    modifique el widget de buscar

--- views/gaceta/index.php
+++ views/gaceta/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use kartik\daterange\DateRangePicker;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\GacetaSearch */
@@ -13,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="gaceta-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php //echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Create Gaceta', ['create'], ['class' => 'btn btn-success']) ?>
@@ -27,7 +28,21 @@ $this->params['breadcrumbs'][] = $this->title;
             'id',
             'asunto',
             'numero',
-            'fecha_publicacion',
+            [
+                'attribute' => 'fecha_publicacion',
+                'value' => 'fecha',
+                'format'=>'raw',
+                'options' => ['style' => 'width: 25%;'],
+                'filter' => DateRangePicker::widget([
+                'model' => $searchModel,
+                'attribute' => 'fecha_publicacion',
+                'useWithAddon'=>false,
+                'convertFormat'=>true,
+                'pluginOptions'=>[
+                    'locale'=>['format'=>'Y-m-d']
+                ],
+                ])
+            ],
             'ruta',
 
             ['class' => 'yii\grid\ActionColumn'],
